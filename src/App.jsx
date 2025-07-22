9964b7a11dca903a4977bcd5388013171f4f2315
angelo-lepore <angelo.lepore94@gmail.com>
2025-07-22 14:52:10 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+// Importa gli stili di Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function App() {
+  return (
+    <>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
